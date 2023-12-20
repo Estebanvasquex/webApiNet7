8fb5b5840fd91bac3d2a9113919cc0d1c6479ddc
Estebanvasquex <53843278+Estebanvasquex@users.noreply.github.com>
2023-12-20 16:57:48 -0500

    Estructura de proyecto web api

---
+++ Core/Entities/TblServiciosMICI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    internal class TblServiciosMICI
+    {
+        [Key]
+        public int id_servicio { get; set; }
+
+        public string Nombre { get; set; }
+        public int Anio { get; set; }
+        public string URL { get; set; }
+        public int Estado { get; set; }
+        public int Trimestre { get; set; }
+        public string Layer { get; set; }
+        public int IdLayer { get; set; }
+        public string RutaRaster { get; set; }
+
+    }
+}
